32f1dc1207b35321ff496e3ab68fcd9ab9fb6141
serhat koç <kocserhat671@gmail.com>
2024-11-20 18:34:40 +0300

    düzenleme

--- app/component/header.jsx
+++ app/component/header.jsx
@@ -101,9 +101,12 @@ export default function Header() {
         <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">KalpEli</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+            <div className='d-flex gap-2'>
+            {!user && (<button className="btn btn-success d-block d-md-none" onClick={() => { handleShowModal(); setIsLogin(true); }}>Kayıt/Giriş</button>)}
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            </div>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav me-auto">
                 <li className="nav-item">
